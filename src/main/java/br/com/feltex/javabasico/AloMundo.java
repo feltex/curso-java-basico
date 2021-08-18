17481cdab116f9231c17ff4a8971a8b836fd5d28
Feltex <professor@feltex.com.br>
2021-08-18 21:00:23 +0100

    Versão inicial

---
+++ src/main/java/br/com/feltex/javabasico/AloMundo.java
@@ -0,0 +1,11 @@
+package br.com.feltex.javabasico;
+
+import javax.swing.*;
+
+public class AloMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Alo Mundo");
+        JOptionPane.showMessageDialog(null,"File!");
+    }
+}
